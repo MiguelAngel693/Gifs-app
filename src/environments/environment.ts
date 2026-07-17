63c233655e344c4eaddf8d8e8f3fb595e84f8c51
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-16 21:45:15 -0300

    feat: agregado archivo de variables de entorno

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  gifsApiKey: 'API_KEY',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
