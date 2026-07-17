7798a14fc7d47a45ac3b1741c8230e4762e768b4
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-16 21:44:01 -0300

    feat: agregado paquete de iconos

--- src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  companyName: 'Gifs',
-  companyName2: 'App',
-  companySlogan: 'Maneja tus gifs',
-
-  gifsApiKey: 'YOUR_API_KEY',
-  giphyUrl: 'https://api.giphy.com/v1',
-};
