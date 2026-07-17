39727c7f15b37b3767c6750a588899c69d9f4a27
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-16 21:29:13 -0300

    feat: agregado menu lateral del dashboard

---
+++ src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { GifService } from '../../services/gifs';
+import { TitleCasePipe } from '@angular/common';
+
+interface MenuOptions{
+  label: string,
+  sublabel: string,
+  route: string,
+  icon: string
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive, TitleCasePipe],
+  templateUrl: './side-menu-options.html',
+})
+export class SideMenuOptions {
+  gifService = inject(GifService);
+
+
+
+  menuOptions: MenuOptions[]=[
+    {
+      icon:'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel:'Gifs populares',
+      route:'/dashboard/trending'
+    },
+    {
+      icon:'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel:'Buscar gifs',
+      route:'/dashboard/search'
+    }
+  ]
+}
