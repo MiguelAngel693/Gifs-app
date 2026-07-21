ba4bcc81dfce44169313e56fb0b830a82644004a
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-20 23:43:09 -0300

    feat: mantenida la posicion del scroll

--- src/app/gifs/pages/trending-page/trending-page.ts
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -1,21 +1,7 @@
-import { Component, computed, ElementRef, inject, signal, viewChild } from '@angular/core';
+import { AfterViewInit, Component, computed, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifsList } from "../../components/gifs-list/gifs-list";
 import { GifService } from '../../services/gifs';
-
-// const imageUrls: string[] = [
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-// ];
+import { ScrollState } from '../../shared/services/scroll state';
 
 @Component({
   selector: 'app-trending-page',
@@ -24,22 +10,33 @@ import { GifService } from '../../services/gifs';
 })
 
 
-export default class TrendingPage {
+export default class TrendingPage implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDiv()?.nativeElement;
+    if (!scrollDiv)
+      return;
+
+    scrollDiv.scrollTop = this.ScrollState.trendingScrollState();
+  }
+
   gifService = inject(GifService);
+  ScrollState = inject(ScrollState);
+
   gifs = this.gifService.trendingGifs();
 
   scrollDiv = viewChild<ElementRef<HTMLDivElement>>('gifList');
-  onScroll(){
+
+
+  onScroll() {
     const scrollDiv = this.scrollDiv()?.nativeElement;
-    if(!scrollDiv)
+    if (!scrollDiv)
       return;
-    const scrollTop:number = scrollDiv.scrollTop;
+    const scrollTop: number = scrollDiv.scrollTop;
     const clientHeight = scrollDiv.clientHeight;
     const scrollHeight: number = scrollDiv.scrollHeight;
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
-
-
-    if(isAtBottom){
+    this.ScrollState.trendingScrollState.set(scrollTop);
+    if (isAtBottom) {
       this.gifService.loadTrendingGifs();
     }
   }
