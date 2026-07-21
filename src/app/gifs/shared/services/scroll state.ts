c8b28d05f21a14ce695bd0c97158a23c9f909c3a
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-20 23:43:31 -0300

    feat: mantenida la posicion del scroll

---
+++ src/app/gifs/shared/services/scroll state.ts
@@ -0,0 +1,9 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ScrollState {
+  trendingScrollState = signal(0);
+  searchScrollState = signal(0);
+}
